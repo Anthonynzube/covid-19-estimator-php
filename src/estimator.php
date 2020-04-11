4cb4afa1057daf792a45e693fd464ff85b80f09a
Anthonynzube <nzubeanthony4@gmail.com>
2020-04-11 20:39:50 +0100

    echo test 1

--- src/estimator.php
+++ src/estimator.php
@@ -1,23 +1,23 @@
 <?php
 
-$data = '{
-    "region": {
-        "name": "Africa",
-        "avgAge": 19.7,
-        "avgDailyIncomeInUSD": 5,
-        "avgDailyIncomePopulation": 0.71
-    },
-    "periodType": "days",
-    "timeToElapse": 58,
-    "reportedCases": 674,
-    "population": 66622705,
-    "totalHospitalBeds": 1380614
-}';
+// $data = '{
+//     "region": {
+//         "name": "Africa",
+//         "avgAge": 19.7,
+//         "avgDailyIncomeInUSD": 5,
+//         "avgDailyIncomePopulation": 0.71
+//     },
+//     "periodType": "days",
+//     "timeToElapse": 58,
+//     "reportedCases": 674,
+//     "population": 66622705,
+//     "totalHospitalBeds": 1380614
+// }';
 
 function covid19ImpactEstimator($data)
 {
-  $decodedData = json_decode(json_encode($data));
-  $decodedData = json_decode($decodedData);
+  $decodedData = json_decode($data);
+  
 
   $timeToElapse = $decodedData->timeToElapse;
   $periodType = $decodedData->periodType;
